084dff458eb38ed43bc22ce6fe327b628d0dd0e8
Mato Ilic <info@matoilic.ch>
2010-04-01 18:02:37 +0200

    version number and copyright updated

--- src/copyright.js
+++ src/copyright.js
@@ -1,5 +1,5 @@
 /*  pDate, version <%= PDATE_VERSION %>
- *  (c) 2009 Mato Ilic
+ *  (c) 2009-2010 Mato Ilic
  *
  *  pDate is freely distributable under the terms of an MIT-style license.
  *  For details, see the pDate web site: http://matoilic.ch/libraries/pDate/
